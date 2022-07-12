136b444bda857d3acf378ec4048799af43d98cd3
lucifer123A <lokeshdulani91@gmail.com>
2022-07-12 17:57:03 +0530

    changed next/Link to next/link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import { config } from '../dapp.config'
 
 export default function Home() {
